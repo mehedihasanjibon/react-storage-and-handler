e361de8c6fb5e27b6a34e905c20d7d1b0437474c
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-25 01:27:54 +0600

    create event handler with parameters and local storage to store data

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Cosmetic.css'
+import { addToDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+
+    const addToCart = (id) => {
+        // console.log('item added' ,id);
+        // localStorage.setItem(id, 1);
+        addToDb(id);
+    }
+
+    // const addToCartWithParam = () => addToCart(id);
+
     return (
         <div className='product'>
             <h2> Buy this: {name}</h2>
             <p> Only for: ${price}</p>
             <p><small> It has id: {id}</small></p>
+            <button onClick={() => addToCart(id)}> Add To Cart </button>
         </div>
     );
 };
